e7a0a6477843b469c3f269bd2e55dfa35e847323
Wilgnne Khawan <wilgnne.kba@gmail.com>
2021-05-10 22:37:03 +0000

    BACKEND_URL prefix in img

--- pages/index.jsx
+++ pages/index.jsx
@@ -62,7 +62,7 @@ export default function Home() {
         >
           Powered by{' '}
           <span className={styles.logo}>
-            <img src="/vercel.svg" alt="Vercel Logo" width="72px" height="16px" />
+            <img src={`${BACKEND_URL}/vercel.svg`} alt="Vercel Logo" width="72px" height="16px" />
           </span>
         </a>
       </footer>
